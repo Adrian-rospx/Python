52124805deab91d9d6996bf3804c9498f2cf3422
Adrian-isp <adrianrosu5400@gmail.com>
2025-03-11 23:27:57 +0200

    built in functions python

---
+++ Functions/builtInFunctions.py
@@ -0,0 +1,22 @@
+# The python language has about 68 built in functions
+# Link for the official documentation:
+# https://docs.python.org/3/library/functions.html
+
+# a few relevant ones are:
+#help()
+
+# alternate bases:
+bin(12)
+oct(20)
+hex(100)
+# type reveal:
+type('12')
+# range iterator:
+range(0, 13, 1)
+# id function
+a = b = 1
+print(id(a))
+print(id(b))
+
+# and many others
+# many functions work with iterators
