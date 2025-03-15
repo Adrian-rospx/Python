26a8696a0c0cb1b9418e5fef3320962351563dac
Adrian-isp <adrianrosu5400@gmail.com>
2025-03-15 23:59:21 +0200

    inheritence

---
+++ OOP/Inheritence.py
@@ -0,0 +1,21 @@
+
+class Vehicle:
+    def __init__(self, speed, year):
+        self.speed = speed
+        self.year = year
+
+    def start(self):
+        print("You started the vehicle")
+
+    def data(self):
+        print(f"The vehicle is from {self.year}.")
+
+class Plane(Vehicle):
+    def fly(self):
+        print("The plane has taken off!")
+
+boeing = Plane(100, 2012)
+
+boeing.start()
+boeing.fly()
+boeing.data()
